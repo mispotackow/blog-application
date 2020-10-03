39d553cc497fc55626b7e1142145221d5c8019ca
mispotackow <potackiw@gmail.com>
2020-10-03 21:10:13 +0300

    Advanced Features2

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -126,6 +126,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'potackiw@gmail.com'
-EMAIL_HOST_PASSWORD = 'password'
+EMAIL_HOST_PASSWORD = 'PASSWORD'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
